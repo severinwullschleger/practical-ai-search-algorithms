f8519ddd40675e2ff02768411b3f61939792cc13
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-03-11 01:22:18 +0100

    nothing special

--- kingsheep/team/swulls/Wolf.java
+++ kingsheep/team/swulls/Wolf.java
@@ -10,47 +10,13 @@ public class Wolf extends SwullsCreature {
 
     protected void think(Type map[][]) {
 
-        if(alive){
+        if (alive) {
             move = getMove(map, getObjectives());
-        }else {
+        } else {
             move = Move.WAIT;
         }
 
-
-        /*
-		TODO
-		YOUR WOLF CODE HERE
-		
-		BASE YOUR LOGIC ON THE INFORMATION FROM THE ARGUMENT map[][]
-		
-		YOUR CODE NEED TO BE DETERMINISTIC. 
-		THAT MEANS, GIVEN A DETERMINISTIC OPPONENT AND MAP THE ACTIONS OF YOUR WOLF HAVE TO BE REPRODUCIBLE
-		
-		SET THE MOVE VARIABLE TO ONE TOF THE 5 VALUES
-        move = Move.UP;
-        move = Move.DOWN;
-        move = Move.LEFT;
-        move = Move.RIGHT;
-        move = Move.WAIT;
-		*/
     }
-
-//    private Move getMove() {
-//        fringe = new LinkedList<Square>();
-//        fringe.push(getCurrentSquare(this));
-//
-//        while (fringe.size() > 0) {
-//
-//            Square s = getLowestCostSquare(fringe);
-//            fringe.remove(s);
-//            if (s.type.equals(objectives[0]))
-//                return s.getNextMoveToGetHere();
-//
-//            map.addAvailableNeighborSquaresToTheFringe(s);
-//        }
-//
-//        return getMove(map, );
-//    }
 
     protected char[] getObjectives() {
         char[] objectives = new char[1];
