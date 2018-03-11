42587b2b2de80366472c1018848f55a60ac8dea7
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-03-11 01:23:32 +0100

    sheep logic from greedy

--- kingsheep/team/swulls/Sheep.java
+++ kingsheep/team/swulls/Sheep.java
@@ -4,28 +4,98 @@ import kingsheep.*;
 
 public class Sheep extends SwullsCreature {
 
+    private boolean noMoreFoodAvailable = false;
+    private Move lastMove;
+    private int counter = 0;
+
     public Sheep(Type type, Simulator parent, int playerID, int x, int y) {
         super(type, parent, playerID, x, y);
     }
 
+    @Override
+    protected char[] getObjectives() {
+        char[] objectives = new char[2];
+        objectives[0] = 'r';
+        objectives[1] = 'g';
+        return objectives;
+    }
+
+    private boolean isSquareSafe(Type map[][], Move move){
+        int x, y;
+
+        if (move == Move.UP){
+            x = this.x;
+            y = this.y - 1;
+        }else if (move == Move.DOWN){
+            x = this.x;
+            y = this.y + 1;
+        }else if (move == Move.LEFT){
+            x = this.x - 1;
+            y = this.y;
+        }else{
+            x = this.x + 1;
+            y = this.y;
+        }
+
+        if (!isCoordinateValid(map, y, x)){
+            return false;
+        }
+
+        Type type = map[y][x];
+
+        if(type == Type.FENCE || type == Type.WOLF2 || type == Type.SHEEP2){
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isCoordinateValid(Type map[][], int y, int x){
+        try{
+            Type type = map[y][x];
+        }catch (ArrayIndexOutOfBoundsException e){
+            return false;
+        }
+        return true;
+    }
+
+    private void fleeFromBadWolf(Type map[][]){
+
+        if (isSquareSafe(map, lastMove)){
+            move = lastMove;
+            return;
+        }
+
+        if (isSquareSafe(map, Move.DOWN)){
+            move = Move.DOWN;
+        }else if (isSquareSafe(map, Move.RIGHT)){
+            move = Move.RIGHT;
+        }else if (isSquareSafe(map, Move.UP)){
+            move = Move.UP;
+        }else{
+            move = Move.LEFT;
+        }
+    }
 
     protected void think(Type map[][]) {
-		/*
-		TODO
-		YOUR SHEEP CODE HERE
-		
-		BASE YOUR LOGIC ON THE INFORMATION FROM THE ARGUMENT map[][]
-		
-		YOUR CODE NEED TO BE DETERMINISTIC. 
-		THAT MEANS, GIVEN A DETERMINISTIC OPPONENT AND MAP THE ACTIONS OF YOUR SHEEP HAVE TO BE REPRODUCIBLE
-		
-		SET THE MOVE VARIABLE TO ONE TOF THE 5 VALUES
-        move = Move.UP;
-        move = Move.DOWN;
-        move = Move.LEFT;
-        move = Move.RIGHT;
-        move = Move.WAIT;
-		*/
-        move = Move.WAIT;
+
+        if(alive && !noMoreFoodAvailable){
+
+            move = getMove(map, getObjectives());
+            if (move == null){
+                move = Move.WAIT;
+            }
+
+            if(move == Move.WAIT){
+                noMoreFoodAvailable = true;
+                fleeFromBadWolf(map);
+            }
+
+        }else{
+            //focusing on escaping the wolf
+            fleeFromBadWolf(map);
+        }
+        lastMove = move;
+
+        ++counter;
     }
 }
