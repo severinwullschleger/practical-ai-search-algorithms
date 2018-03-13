8ceb2d59d2cb19a13fac459894497db5e76d6ff2
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-03-13 09:51:37 +0100

    flee from wolf

--- kingsheep/team/swulls/Sheep.java
+++ kingsheep/team/swulls/Sheep.java
@@ -6,10 +6,33 @@ public class Sheep extends SwullsCreature {
 
     private boolean noMoreFoodAvailable = false;
     private Move lastMove;
-    private int counter = 0;
 
     public Sheep(Type type, Simulator parent, int playerID, int x, int y) {
         super(type, parent, playerID, x, y);
+        counter = 0;
+    }
+
+    @Override
+    protected void think(Type map[][]) {
+
+        counter++;
+
+        if(alive && !noMoreFoodAvailable){
+
+            move = getMove(map, getObjectives());
+            if (move == null){
+                move = Move.WAIT;
+            }
+
+            if(move == Move.WAIT){
+                noMoreFoodAvailable = true;
+                fleeFromBadWolf(map);
+            }
+        }else{
+            //focusing on escaping the wolf
+            fleeFromBadWolf(map);
+        }
+        lastMove = move;
     }
 
     @Override
@@ -18,44 +41,6 @@ public class Sheep extends SwullsCreature {
         objectives[0] = 'r';
         objectives[1] = 'g';
         return objectives;
-    }
-
-    private boolean isSquareSafe(Type map[][], Move move){
-        int x, y;
-
-        if (move == Move.UP){
-            x = this.x;
-            y = this.y - 1;
-        }else if (move == Move.DOWN){
-            x = this.x;
-            y = this.y + 1;
-        }else if (move == Move.LEFT){
-            x = this.x - 1;
-            y = this.y;
-        }else{
-            x = this.x + 1;
-            y = this.y;
-        }
-
-        if (!isCoordinateValid(map, y, x)){
-            return false;
-        }
-
-        Type type = map[y][x];
-
-        if(type == Type.FENCE || type == Type.WOLF2 || type == Type.SHEEP2){
-            return false;
-        }
-        return true;
-    }
-
-    private boolean isCoordinateValid(Type map[][], int y, int x){
-        try{
-            Type type = map[y][x];
-        }catch (ArrayIndexOutOfBoundsException e){
-            return false;
-        }
-        return true;
     }
 
     private void fleeFromBadWolf(Type map[][]){
@@ -76,26 +61,39 @@ public class Sheep extends SwullsCreature {
         }
     }
 
-    protected void think(Type map[][]) {
+    private boolean isSquareSafe(Type map[][], Move move){
+        int x, y;
 
-        if(alive && !noMoreFoodAvailable){
-
-            move = getMove(map, getObjectives());
-            if (move == null){
-                move = Move.WAIT;
-            }
-
-            if(move == Move.WAIT){
-                noMoreFoodAvailable = true;
-                fleeFromBadWolf(map);
-            }
-
-        }else{
-            //focusing on escaping the wolf
-            fleeFromBadWolf(map);
+        if (move == Move.UP){
+            x = this.x;
+            y = this.y - 1;
+        }else if (move == Move.DOWN){
+            x = this.x;
+            y = this.y + 1;
+        }else if (move == Move.LEFT){
+            x = this.x - 1;
+            y = this.y;
+        }else if (move == Move.RIGHT) {
+            x = this.x + 1;
+            y = this.y;
+        }else {
+            x = this.x;
+            y = this.y;
         }
-        lastMove = move;
 
-        ++counter;
+        if (!isCoordinateValid(map, y, x)){
+            return false;
+        }
+
+        Type type = map[y][x];
+
+        if(type == Type.FENCE )//|| type == Type.WOLF2 || type == Type.SHEEP2)
+            return false;
+        else if (this.type == Type.SHEEP1 && type == Type.WOLF2)
+            return false;
+        else if (this.type == Type.SHEEP2 && type == Type.WOLF1)
+            return false;
+        else
+            return true;
     }
 }
