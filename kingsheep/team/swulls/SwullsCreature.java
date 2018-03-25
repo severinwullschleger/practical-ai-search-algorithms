ab43687ff653a339920fe970405a2382fd4dd003
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-03-25 13:38:04 +0200

    changed cutOffDepth

--- kingsheep/team/swulls/SwullsCreature.java
+++ kingsheep/team/swulls/SwullsCreature.java
@@ -33,7 +33,7 @@ public abstract class SwullsCreature extends Creature {
     protected Move getMove(Type map[][], char[] objective) {
         visitedSquares = new HashMap<String, Square>();
         squareQueue = new ArrayList<Square>();
-        cutOffDepth = 8;
+        cutOffDepth = 7;
         findingObjectiveMode = false;
 
         this.map = map;
